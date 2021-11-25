c1bab648e4b2362c2766187b8d6522a4eef377cf
ArnasOFC <thexamex@gmail.com>
2021-11-25 22:16:41 +0100

    V2.0

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 'use strict';
 
-const btnGenerate = document.querySelector('.btn-generate');
+const btnHex = document.querySelector('.btn-generate-hex');
+const btnRgba = document.querySelector('.btn-generate-rgba');
 const hexColor = document.querySelector('.hex-color');
 
-const randomNumber = () => `#${100000 + Math.trunc(Math.random() * 900000)}`;
+const randomNumb = (from, to) => from + Math.trunc(Math.random() * to);
 
-btnGenerate.addEventListener('click', function () {
-  hexColor.textContent = randomNumber();
-  document.querySelector('body').style.backgroundColor = randomNumber();
+btnHex.addEventListener('click', function () {
+  hexColor.textContent = `#${randomNumb(100000, 900000)}`;
+  document.querySelector('body').style.backgroundColor = `#${randomNumb(100000, 900000)}`;
+});
+
+btnRgba.addEventListener('click', function () {
+  let a = randomNumb(100, 255);
+  let b = randomNumb(100, 255);
+  let c = randomNumb(100, 255);
+  hexColor.textContent = `Rgb(${a},${b},${c})`;
+  document.querySelector('body').style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
 });
